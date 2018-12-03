b5654a16baa981c82a503cf684e1d7d885f8f216
Ricky Cordero <8922867+RickyCordero@users.noreply.github.com>
2018-12-02 20:28:42 -0800

    Testing spotify redirect uri changes

--- routes/host.js
+++ routes/host.js
@@ -20,13 +20,13 @@ const spotifyApi = new SpotifyWebApi();
 const client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
 
-let redirect_uri;
+let redirect_uri = `https://auxme.io/host/spotify/callback/`; // Your redirect uri
 
-if (process.env.ENVIRONMENT == 'development') {
-    redirect_uri = `http://localhost:${process.env.PORT}/host/spotify/callback/`;
-} else {
-    redirect_uri = `https://auxme.io/host/spotify/callback/`; // Your redirect uri
-}
+// if (process.env.ENVIRONMENT == 'development') {
+//     redirect_uri = `http://localhost:${process.env.PORT}/host/spotify/callback/`;
+// } else {
+//     redirect_uri = `https://auxme.io/host/spotify/callback/`; // Your redirect uri
+// }
 
 const stateKey = 'spotify_auth_state';
 
