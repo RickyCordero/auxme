29d1b2047ee8742a5dacbfc621dfed7e0b2f865c
Ricky Cordero <8922867+RickyCordero@users.noreply.github.com>
2018-12-06 14:50:11 -0800

    Fixed registration redirect

--- routes/users.js
+++ routes/users.js
@@ -134,7 +134,7 @@ module.exports = function (io) {
       req.flash('success', 'You are now registered and can login');
 
       res.location('/');
-      res.redirect('/');
+      res.redirect('/users/login');
     }
   });
 
